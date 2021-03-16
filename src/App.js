791be8672ec884fdecd4686391512660063364b0
MaliMeghana <meghanamagare96@gmail.com>
2021-03-17 01:17:51 +0530

    CDAC React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
+import AdminField from "./components/AdminField";
+import Admin from "./components/Admin";
+import SupAdminField from "./components/SupAdminField";
+import { Route, BrowserRouter as Router } from "react-router-dom";
+import UpdateStudent from "./components/UpdateStudent";
+import Student from "./components/Student";
+import Login from "./components/Login";
+import Feedback from "./components/Feedback";
+import Notice from "./components/Notice";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <Router>
+        <Header />
+        <div className="main">
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <Route path="/admin">
+            <AdminField />
+          </Route>
+          <Route path="/adminnext">
+            <Admin />
+          </Route>
+          <Route path="/supadmin">
+            <SupAdminField />
+          </Route>
+          <Route path="/student">
+            <Student />
+          </Route>
+          <Route path="/updateStudent">
+            <UpdateStudent />
+          </Route>
+          <Route path="/feedback">
+            <Feedback />
+          </Route>
+          <Route path="/notice">
+            <Notice />
+          </Route>
+        </div>
+      </Router>
     </div>
   );
 }
